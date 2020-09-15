42528f469d5ec5025f8affd8fb3c64215782e5ad
Tahsin <62146852+TahsinTariq@users.noreply.github.com>
2020-09-15 16:42:05 +0600

    Update sketch.js

--- P5_Sketches/P5_Web_Collection/VoronoiGeneration/sketch.js
+++ P5_Sketches/P5_Web_Collection/VoronoiGeneration/sketch.js
@@ -1,9 +1,40 @@
+let v;
 function setup() {
-	createCanvas(640, 480);
+	createCanvas(windowWidth, windowHeight-15);
 	background(0);
+	noStroke();
+	colorMode(HSB, 360,100,100)
+	v = new voronoi(150);
 }
 
 function draw() {
-	fill(random(255))
-	ellipse(width/2, height/2, 100, 100)
+	if(frameCount%100 == 0)
+		v = new voronoi(150);
+		background(0);
+		v.show();
+	// noLoop();
+	// fill(0,100,0, 50);
+	// rect(0,0,width, height);
+}
+class voronoi{
+	constructor(number){
+		this.number = number;
+		this.hue = [];
+		this.r = []
+		this.x = [];
+		this.y = [];
+		for (var i = 0; i < this.number; i++) {
+			this.hue.push(random(0,50))
+			this.r.push(random(50, 200))
+			this.x.push(random(width))
+			this.y.push(random(height))
+		}
+		console.log(this.x[0])
+	}
+	show(){
+		for (var i = 0; i < this.number; i++) {
+			fill(this.hue[i], 80, 90);
+			ellipse(this.x[i], this.y[i], this.r[i], this.r[i]);
+		}
+	}
 }
