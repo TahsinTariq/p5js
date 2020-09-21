77b9ca875f08154a5a14ecd0fc7bf0848b319b9a
Tahsin <62146852+TahsinTariq@users.noreply.github.com>
2020-09-21 23:58:00 +0600

    added test i guess

--- P5_Sketches/P5_Web_Collection/EightPuzzle/sketch.js
+++ P5_Sketches/P5_Web_Collection/EightPuzzle/sketch.js
@@ -1,45 +1,34 @@
-// import { Heap } from 'heap-js';
-const numbers = [2, 3, 7, 5];
-
-Heap.heapify(numbers);
-console.log(numbers); //> [ 2, 3, 5, 7 ]
-
-Heap.heappush(numbers, 1);
-console.log(numbers);
 let r;
 let path = [];
 let board = [
 	["0", "5", "4"],
     ["1", "6", "2"],
     ["7", "3", "8"]
+]
+let goal = [
+	["1", "2", "3"],
+    ["4", "5", "6"],
+    ["7", "8", "0"]
 ];
-<<<<<<< Updated upstream
-actions = [
-	up = [0,1],
-	down = [0, -1],
-	left = [-1,0],
-	right = [1,0]
-];
-=======
 let actions = {
 	up   : [ 0,-1],
 	down : [ 0, 1],
 	left : [-1, 0],
 	right: [ 1, 0]
 };
->>>>>>> Stashed changes
 function setup() {
 	createCanvas(windowWidth, windowHeight);
 	background(0);
 	r = 100;
 	textAlign(CENTER, CENTER);
+	rectMode(CENTER);
 	textSize(100);
 	button = createButton('click me');
 	button.position(19, 19);
 	button.mousePressed(null);
-	for(action in actions){
-		console.log(action);
-	}
+	// for(let[action, val] of Object.entries(actions)){
+	// 	console.log(action, val);
+	// }
 }
 
 function windowResized() {
@@ -47,11 +36,16 @@ function windowResized() {
 }
 
 function draw() {
-
-	for (let i = 0; i< board.length;i++){
-		for (let j = 0; j< board[1].length;j++){
-			rect(i*r, j*r, r,r);
-			if (board[j][i] != 0){
+	fill(255);
+	rect(0,0,2*r*board.length, 2*r*board.length);
+	for (j in board){
+		for (i in board[j]){
+			let xpos = j*r +r/2;
+			let ypos = i*r +r/2;
+			noFill();
+			rect(xpos, ypos, r,r);
+			fill(255,0,0);
+			if(board[j][i] != 0 ){
 				text(board[j][i], i*r+r/2, j*r+r/2);
 			}
 		}
@@ -59,14 +53,7 @@ function draw() {
 }
 
 function keyPressed(){
-	for (let i = 0; i< board.length;i++){
-		for (let j = 0; j< board[1].length;j++){
-			if(keyCode === UP_ARROW || key =='w'){
 
-<<<<<<< Updated upstream
-			}
-			if(keyCode === DOWN_ARROW || key =='s'){
-=======
 	if(keyCode === UP_ARROW || key =='w'){
 		swap(actions.up);
 	}
@@ -79,114 +66,96 @@ function keyPressed(){
 	if(keyCode === RIGHT_ARROW || key =='d'){
 		swap(actions.right);
 	}
-	// if(key == 'r'){
-	// 	b = boardtostring(board)
- //        if parity(b)%2 == parity(goal)%2:
- //            console.log('SEARCHING ... ... ... ...')
- //            AStar(b, goal)
- //        else: console.log("UNSOLVABLE")
-	// }
-}
->>>>>>> Stashed changes
-
-			}
-<<<<<<< Updated upstream
-			if(keyCode === LEFT_ARROW || key =='a'){
-
-			}
-			if(keyCode === RIGHT_ARROW || key =='d'){
-
-			}
+	if(key == 'r'){
+		b = boardtostring(board)
+        if parity(b)%2 == parity(goal)%2:
+            console.log('SEARCHING ... ... ... ...')
+            AStar(b, goal)
+        else: console.log("UNSOLVABLE")
+	}
 }
 
-// function swap( a,  b,  c,  d){
-//     t = board[a][b]
-//     board[a][b]= board[c][d]
-//     board[c][d] = t
-// }
-=======
+function swap(action){
+	for (j in board){
+		for (i in board[i]){
+			if (board[i][j] == 0){
+				[x, y] = math.add([j, i], action);
+				if((-1 < x && x < board.length)&&(-1 < y && y < board.length)){
+					board[i][j] = board[y][x];
+					board[y][x] = 0;
+					return 0;
+				}
+			}
 		}
 	}
 }
 
-// function parity(S):
-//     S = S.replace("0","")
-//     count = 0
-//     for (i in board){
-// 		for (j = i; j<board.length; j++)
-//             if (S[i]>S[j])
-//                 count +=1
-//     return count
+function parity(S):
+    S = S.replace("0","")
+    count = 0
+    for (i in board){
+		for (j = i; j<board.length; j++)
+            if (S[i]>S[j])
+                count +=1
+    return count
 
-// class node{
-// 	constructor(current, parent, gcost){
-// 		this.parent = parent;
-// 		this.gcost = gcost;
-// 		this.h = getHeuristic(current);
-// 		this.child = generatechild(current);
-// 	}
-// }
+class node{
+	constructor(current, parent, gcost){
+		this.parent = parent;
+		this.gcost = gcost;
+		this.h = getHeuristic(current);
+		this.child = generatechild(current);
+	}
+}
 
-// function route(v1, v2):
-//     if parents[v2] != 'NONE':
-//         route(v1, parents[v2])
-//     print(v2)
-// 	path.push(v2)
+function route(v1, v2):
+    if parents[v2] != 'NONE':
+        route(v1, parents[v2])
+    print(v2)
+	path.push(v2)
 
-// def boardtostring(Board):
-//     string = ""
-//     for (i in Board){
-//         for j in i:
-//             string+=j
-//     }
-//     return string
+def boardtostring(Board):
+    string = ""
+    for (i in Board){
+        for j in i:
+            string+=j
+    }
+    return string
 
-// function AStar(v1, v2){
-// 	let heapq = require('heapq');
-//     parents = {}
-//     searched = []
-//     fcost = {}
-//     queue = {}
+function AStar(v1, v2){
+    parents = {}
+    searched = []
+    fcost = {}
+    queue = {}
 
-//     parents[v1] = "NONE"
-//     gcost[v1] = 0
-//     fcost[v1] = h(v1)
-//     queue[v1] = fcost[v1]
-//     def lowestCost(d1):
-//         hash_ = generatechild(d1);
-//         for n, c in hash_.items():
-//             if c + gcost[d1] < gcost[n] and n not in searched:
-//                 gcost[n] = c + gcost[d1]
-//                 fcost[n] = gcost[n] + h(n)
-//                 parents[n] = d1
-//                 if n not in queue.keys():
-//                     queue[n] = fcost[n]
-//     while queue:
-//         x = sorted(queue, key=queue.get, reverse=True)
-//         node= x.pop()
-//         queue.pop(node)
-//         searched.append(node)
-//         if node == v2:
-//             try{
-//                 print("found");
-//                 rout(v1, v2);
-//         	}
-//             catch{
-//                 print('No path exists');
-//             }
-//             return None
-//         lowestCost(node)
-//     print('NO path Found')
-// }
+    parents[v1] = "NONE"
+    gcost[v1] = 0
+    fcost[v1] = h(v1)
+    queue[v1] = fcost[v1]
+    def lowestCost(d1):
+        hash_ = generatechild(d1);
+        for n, c in hash_.items():
+            if c + gcost[d1] < gcost[n] and n not in searched:
+                gcost[n] = c + gcost[d1]
+                fcost[n] = gcost[n] + h(n)
+                parents[n] = d1
+                if n not in queue.keys():
+                    queue[n] = fcost[n]
+    while queue:
+        x = sorted(queue, key=queue.get, reverse=True)
+        node= x.pop()
+        queue.pop(node)
+        searched.append(node)
+        if node == v2:
+            try{
+                print("found");
+                rout(v1, v2);
+        	}
+            catch{
+                print('No path exists');
+            }
+            return None
+        lowestCost(node)
+    print('NO path Found')
+}
 
-// let {
-// 	heapify ,
-// 	heappop ,
-// 	heappush ,
-// 	heappushpop ,
-// 	heapreplace ,
-// 	merge ,
-// 	nlargest ,
-// 	nsmallest ,
-// } = heapq;
->>>>>>> Stashed changes
