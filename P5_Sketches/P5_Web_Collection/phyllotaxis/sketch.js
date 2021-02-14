0bbe6cafc220a6e6db0d3d32473c750b7c38c83e
Tahsin <tahsinbanna@gmail.com>
2021-02-14 19:30:12 +0600

    made a plotter

--- P5_Sketches/P5_Web_Collection/phyllotaxis/sketch.js
+++ P5_Sketches/P5_Web_Collection/phyllotaxis/sketch.js
@@ -1,31 +1,44 @@
-n = 0
+n = 1
 degree = 137.5
 s = 6
 
+colors = []
 
 function setup() {
 	// createCanvas(640, 480);
 	createCanvas(windowWidth, windowHeight);
 	background(0);
 	colorMode(HSB, 360, 100, 100)
+	for (i = 0; i < 20; i++) {
+		colors.push(color(random(160, 330), 80, 90))
+	}
 }
+
 function windowResize() {
 	resizeCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
-	push()
-	translate(width / 2, height / 2)
-	pos = ptxPos(degree, s, n)
-	fill(map(n % 5, 0, 10, 0, 300), 80, 90)
-	ellipse(pos[0], pos[1], 10, 10)
-	n += 1
-	pop()
+	for (i = 0; i < 25; i++) {
+		push()
+		translate(width / 2, height / 2)
+		pos = ptxPos(degree, s, n)
+		// fill(map(n % 5, 0, 10, 0, 300), 80, 90)
+		fill(colors[n % 20])
+		ellipse(pos[0], pos[1], 10, 10)
+		n += 1
+		if (pos[0] + pos[1] > width / 2 + height / 2) {
+			noLoop()
+			print("paused")
+		}
+		pop()
+	}
 }
 
 function ptxPos(degree, scale, count) {
 	angle = count * radians(degree)
 	r = scale * sqrt(count)
+	// r = -scale * sqrt(count) * (2 - 2 * sin(angle) + sin(angle) * sqrt(abs(cos(angle)) / (sin(angle) + 1.5)))
 	x = r * cos(angle)
 	y = r * sin(angle)
 	return [x, y]
@@ -34,6 +47,7 @@ function ptxPos(degree, scale, count) {
 function mousePressed() {
 	n = 0
 	background(0)
+	loop()
 	// s = map(mouseX, 0, width, 2, 10)
 	// print(s)
 }
---
+++ P5_Sketches/P5_Web_Collection/plotter/sketch.js
@@ -0,0 +1,46 @@
+// n =
+function setup() {
+	// createCanvas(640, 480);
+	createCanvas(windowWidth, windowHeight);
+	background(0);
+	colorMode(HSB, 360, 100, 100)
+}
+function windowResize() {
+	resizeCanvas(windowWidth, windowHeight);
+}
+
+function draw() {
+	background(0)
+	push()
+	translate(width / 2, height / 2)
+	beginShape()
+	for (let i = 0; i < 360; i += 0.5) {
+		// s = map(mouseX, 0, width, 1, 100)
+		// print(s)
+		m = min(width, height) / 50
+		s = map(sin(frameCount / 10), -1, 1, m / 2, m)
+		pos = cartToParametric(i, s)
+		fill(0, 80, 90)
+		vertex(pos[0], pos[1])
+
+	}
+	endShape(CLOSE)
+	pop()
+
+}
+
+function polarToCart(degree, scale) {
+	angle = radians(degree)
+	r = - scale * (2 - 2 * sin(angle) + sin(angle) * sqrt(abs(cos(angle)) / (sin(angle) + 1.5)))
+	x = r * cos(angle)
+	y = r * sin(angle)
+	return [x, y]
+}
+
+function cartToParametric(degree, scale) {
+	angle = radians(degree)
+	// r = - scale * (2 - 2 * sin(angle) + sin(angle) * sqrt(abs(cos(angle)) / (sin(angle) + 1.5)))
+	x = scale * 16 * pow(sin(angle), 3)
+	y = -scale * (13 * cos(angle) - 5 * cos(2 * angle) - 2 * cos(3 * angle) - cos(4 * angle))
+	return [x, y]
+}
