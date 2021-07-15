0e7a5c2715ba6f11edbc2ab5d10287eca2ab252d
Tahsin <tahsinbanna@gmail.com>
2021-07-15 16:25:55 +0600

    fixed clipping issues

--- P5_Sketches/P5_Web_Collection/obs_bg/sketch.js
+++ P5_Sketches/P5_Web_Collection/obs_bg/sketch.js
@@ -1,5 +1,5 @@
 var startTime;
-var target_minutes = 10;
+var target_minutes = 15;
 var target = minuteToFrame(target_minutes);
 function setup() {
 	// createCanvas(640, 480);
@@ -9,6 +9,7 @@ function setup() {
 	// startTime = performance.now();
 	// get start time
 	// console.log(startTime);
+	// console.log(target);
 	colorMode(HSB, 360, 100, 100);
 }
 function windowResize() {
@@ -18,12 +19,14 @@ function windowResize() {
 function draw() {
 	// end = performance.now();
 	c1 = color(0, 90, 90);
-	c2 = color(360, 85, 90);
+	c2 = color(359, 85, 90);
 	// time_passed = Math.round((end - startTime) / 1000)
 	// console.log(`Time passed: ${time_passed} seconds`);
-	// console.log(target_minutes);
-	c = lerpColor(c1, c2, map(frameCount % target, 0, target, 0, 1));
-	// console.log(`c: ${c}`);
+	// console.log(frameCount);
+	val = map(frameCount % target, 1, target, 0, 1);
+	// console.log(val);
+	c = lerpColor(c1, c2, val);
+	// console.log(`c: ${hue(c)}`);
 	background(c);
 }
 
